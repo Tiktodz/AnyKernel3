0de858e2a1ea5b0f36fe840ee08b09815a6c2625
RyuujiX <saputradenny712@gmail.com>
2021-05-07 13:39:23 +0700

    Add Method for X00TD
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- anykernel-real.sh
+++ anykernel-real.sh
@@ -29,8 +29,15 @@ supported.versions=11
 supported.patchlevels=
 '; } # end properties
 
+# Installation Method
+X00TD=0
+
 # shell variables
+if [ "$X00TD" = "1" ];then
+block=/dev/block/platform/soc/c0c4000.sdhci/by-name/boot;
+else
 block=/dev/block/bootdevice/by-name/boot;
+fi
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -39,11 +46,22 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+# Mount partitions as rw
+mount /system;
+mount /vendor;
+mount -o remount,rw /system;
+mount -o remount,rw /vendor;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
+if [ "$X00TD" = "1" ];then
+chmod -R 750 $ramdisk/*;
+chmod -R 755 $ramdisk/sbin;
+chmod -R root:root $ramdisk/*;
+else
 set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+set_perm_recursive 0 0 755 755 $ramdisk/init* $ramdisk/sbin;
+fi
 
 
 ## AnyKernel install
@@ -51,8 +69,31 @@ dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
+#Remove old kernel stuffs from ramdisk
+if [ "$X00TD" = "1" ];then
+ rm -rf $ramdisk/init.special_power.sh
+ rm -rf $ramdisk/init.darkonah.rc
+ rm -rf $ramdisk/init.spectrum.rc
+ rm -rf $ramdisk/init.spectrum.sh
+ rm -rf $ramdisk/init.boost.rc
+ rm -rf $ramdisk/init.trb.rc
+ rm -rf $ramdisk/init.azure.rc
+ rm -rf $ramdisk/init.PBH.rc
+ rm -rf $ramdisk/init.Pbh.rc
+ rm -rf $ramdisk/init.overdose.rc
+fi
+
 backup_file init.rc;
+if [ "$X00TD" = "1" ];then
+remove_line init.rc "import /init.darkonah.rc";
+remove_line init.rc "import /init.spectrum.rc";
+remove_line init.rc "import /init.boost.rc";
+remove_line init.rc "import /init.trb.rc"
+remove_line init.rc "import /init.azure.rc"
+remove_line init.rc "import /init.PbH.rc"
+remove_line init.rc "import /init.Pbh.rc"
+remove_line init.rc "import /init.overdose.rc"
+else
 replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
 
 # init.tuna.rc
@@ -68,41 +109,72 @@ patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=
 append_file fstab.tuna "usbdisk" fstab;
 
 # remove spectrum profile
-if [ -e $ramdisk/init.spectrum.rc ];then
-  rm -rf $ramdisk/init.spectrum.rc
-  ui_print "delete /init.spectrum.rc"
+	if [ -e $ramdisk/init.spectrum.rc ];then
+	  rm -rf $ramdisk/init.spectrum.rc
+	  ui_print "delete /init.spectrum.rc"
+	fi
+	if [ -e $ramdisk/init.spectrum.sh ];then
+	  rm -rf $ramdisk/init.spectrum.sh
+	  ui_print "delete /init.spectrum.sh"
+	fi
+	if [ -e $ramdisk/sbin/init.spectrum.rc ];then
+	  rm -rf $ramdisk/sbin/init.spectrum.rc
+	  ui_print "delete /sbin/init.spectrum.rc"
+	fi
+	if [ -e $ramdisk/sbin/init.spectrum.sh ];then
+	  rm -rf $ramdisk/sbin/init.spectrum.sh
+	  ui_print "delete /sbin/init.spectrum.sh"
+	fi
+	if [ -e $ramdisk/etc/init.spectrum.rc ];then
+	  rm -rf $ramdisk/etc/init.spectrum.rc
+	  ui_print "delete /etc/init.spectrum.rc"
+	fi
+	if [ -e $ramdisk/etc/init.spectrum.sh ];then
+	  rm -rf $ramdisk/etc/init.spectrum.sh
+	  ui_print "delete /etc/init.spectrum.sh"
+	fi
+	if [ -e $ramdisk/init.aurora.rc ];then
+	  rm -rf $ramdisk/init.aurora.rc
+	  ui_print "delete /init.aurora.rc"
+	fi
+	if [ -e $ramdisk/sbin/init.aurora.rc ];then
+	  rm -rf $ramdisk/sbin/init.aurora.rc
+	  ui_print "delete /sbin/init.aurora.rc"
+	fi
+	if [ -e $ramdisk/etc/init.aurora.rc ];then
+	  rm -rf $ramdisk/etc/init.aurora.rc
+	  ui_print "delete /etc/init.aurora.rc"
+	fi
 fi
-if [ -e $ramdisk/init.spectrum.sh ];then
-  rm -rf $ramdisk/init.spectrum.sh
-  ui_print "delete /init.spectrum.sh"
+
+# rearm perfboostsconfig.xml
+if [ ! -f /vendor/etc/perf/perfboostsconfig.xml ]; then
+	mv /vendor/etc/perf/perfboostsconfig.xml.bak /vendor/etc/perf/perfboostsconfig.xml;
+	mv /vendor/etc/perf/perfboostsconfig.xml.bkp /vendor/etc/perf/perfboostsconfig.xml;
 fi
-if [ -e $ramdisk/sbin/init.spectrum.rc ];then
-  rm -rf $ramdisk/sbin/init.spectrum.rc
-  ui_print "delete /sbin/init.spectrum.rc"
+
+# rearm commonresourceconfigs.xml
+if [ ! -f /vendor/etc/perf/commonresourceconfigs.xml ]; then
+	mv /vendor/etc/perf/commonresourceconfigs.xml.bak /vendor/etc/perf/commonresourceconfigs.xml;
+	mv /vendor/etc/perf/commonresourceconfigs.xml.bkp /vendor/etc/perf/commonresourceconfigs.xml;
 fi
-if [ -e $ramdisk/sbin/init.spectrum.sh ];then
-  rm -rf $ramdisk/sbin/init.spectrum.sh
-  ui_print "delete /sbin/init.spectrum.sh"
+
+# rearm targetconfig.xml
+if [ ! -f /vendor/etc/perf/targetconfig.xml ]; then
+	mv /vendor/etc/perf/targetconfig.xml.bak /vendor/etc/perf/targetconfig.xml;
+	mv /vendor/etc/perf/targetconfig.xml.bkp /vendor/etc/perf/targetconfig.xml;
 fi
-if [ -e $ramdisk/etc/init.spectrum.rc ];then
-  rm -rf $ramdisk/etc/init.spectrum.rc
-  ui_print "delete /etc/init.spectrum.rc"
+
+# rearm targetresourceconfigs.xml
+if [ ! -f /vendor/etc/perf/targetresourceconfigs.xml ]; then
+	mv /vendor/etc/perf/targetresourceconfigs.xml.bak /vendor/etc/perf/targetresourceconfigs.xml;
+	mv /vendor/etc/perf/targetresourceconfigs.xml.bkp /vendor/etc/perf/targetresourceconfigs.xml;
 fi
-if [ -e $ramdisk/etc/init.spectrum.sh ];then
-  rm -rf $ramdisk/etc/init.spectrum.sh
-  ui_print "delete /etc/init.spectrum.sh"
-fi
-if [ -e $ramdisk/init.aurora.rc ];then
-  rm -rf $ramdisk/init.aurora.rc
-  ui_print "delete /init.aurora.rc"
-fi
-if [ -e $ramdisk/sbin/init.aurora.rc ];then
-  rm -rf $ramdisk/sbin/init.aurora.rc
-  ui_print "delete /sbin/init.aurora.rc"
-fi
-if [ -e $ramdisk/etc/init.aurora.rc ];then
-  rm -rf $ramdisk/etc/init.aurora.rc
-  ui_print "delete /etc/init.aurora.rc"
+
+# rearm powerhint.xml
+if [ ! -f /vendor/etc/powerhint.xml ]; then
+	mv /vendor/etc/powerhint.xml.bak /vendor/etc/powerhint.xml;
+	mv /vendor/etc/powerhint.xml.bkp /vendor/etc/powerhint.xml;
 fi
 
 # end ramdisk changes
