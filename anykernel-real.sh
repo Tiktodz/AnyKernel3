14451a0263cfbd49733e0a96fce70cf49c9b7fc3
Meisie Widuri <ewprjkt@proton.me>
2023-11-02 16:44:00 +0700

    Update supported.versions

--- anykernel-real.sh
+++ anykernel-real.sh
@@ -23,7 +23,7 @@ device.name2=X01BDA
 device.name3=Zenfone Max Pro M2 (X01BD)
 device.name4=ASUS_X01BD
 device.name5=ASUS_X01BDA
-supported.versions=9-12
+supported.versions=9-14
 supported.patchlevels=
 '; } # end properties
 
